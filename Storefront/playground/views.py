397f88adc155af817db82ab3fc3d570a6200a28b
wasee187 <waseesarwar187@gmail.com>
2025-01-21 22:58:42 +0200

    managers and query set

--- Storefront/playground/views.py
+++ Storefront/playground/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from store.models import Product
 
 def say_hello(request):
+    # pylint: disable=no-member
+    query_set = Product.objects.all()
+    for product in query_set: 
+        print(product)
 
     return render(request, 'hello.html', {'name': 'Wasee'})
