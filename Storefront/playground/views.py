924ebbcf3c3e2aa984a3e3e18273cd92e268acde
wasee187 <waseesarwar187@gmail.com>
2025-02-17 20:29:30 +0200

    filtering objects

--- Storefront/playground/views.py
+++ Storefront/playground/views.py
@@ -5,9 +5,17 @@ from store.models import Product
 
 def say_hello(request):
     # pylint: disable=no-member
-    try:
-        product = Product.objects.get(pk=0)
-    except ObjectDoesNotExist:
-        pass
+    #product = Product.objects.filter(pk=0).first() # result = none
+    #exists = Product.objects.filter(pk=0).exists() # return boolean
+    #try:
+    #    product = Product.objects.get(pk=0)
+    #except ObjectDoesNotExist:
+     #   pass
+    #queryset = Product.objects.filter(unit_price__gt = 20)
+    #queryset = Product.objects.filter(unit_price__range = (20,30))
+    #queryset = Product.objects.filter(collection__id=2)
+    #queryset = Product.objects.filter(collection__id__range=(1,2,3))
+    #queryset = Product.objects.filter(title__icontains='coffee')
+    queryset = Product.objects.filter(description__isnull =True)
 
-    return render(request, 'hello.html', {'name': 'Wasee'})
+    return render(request, 'hello.html', {'name': 'Wasee', 'products':list(queryset)})
