50ca0de39dc0ff602eda530da105539bcfca25cd
wasee187 <waseesarwar187@gmail.com>
2025-02-18 09:30:29 +0200

    Aggregating objects

--- Storefront/playground/views.py
+++ Storefront/playground/views.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from store.models import Product, Order
 
 def say_hello(request):
     # pylint: disable=no-member
-    #queryset = Product.objects.prefetch_related('promotions').select_related('collection').all()
-
-    #exercise Get the last 5 orders with their customer and items (incl product) 
-
-    #my_solution 
-
-    queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
-    return render(request, 'hello.html', {'name': 'Wasee', 'orders':list(queryset)})
+    result = Product.objects.filter('collection__id =1').aggregate(count=Count('id'), min_price = Min('unit_price'))
+    return render(request, 'hello.html', {'name': 'Wasee', 'result':result})
