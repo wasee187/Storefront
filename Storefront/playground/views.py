887f8e149318059a77f7e7351547888eb5e57214
wasee187 <waseesarwar187@gmail.com>
2025-02-18 20:43:52 +0200

    Transaction

--- Storefront/playground/views.py
+++ Storefront/playground/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from django.db import transaction
 from store.models import Product, Order, OrderItem, Product, Customer,Collection
 
 def say_hello(request):
-    # pylint: disable=no-member    
-    collection = Collection(pk=11)
-    collection.delete()
+    # pylint: disable=no-member  
 
-    Collection.objects.filter(id__gt = 5).delete()
+    with transaction.atomic():  
+        order = Order()
+        order.customer_id = 1
+        order.save()
+
+        item = OrderItem()
+        item.quantity = 1
+        item.unit_price = 10
+        item.product_id = 1
+        item.save()
 
     return render(request, 'hello.html', {'name': 'Wasee'})
