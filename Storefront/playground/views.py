706d770a7d8837c92b9dab184297f6d65c8164b9
wasee187 <waseesarwar187@gmail.com>
2025-02-18 19:05:40 +0200

    Custom Managers

--- Storefront/playground/views.py
+++ Storefront/playground/views.py
@@ -5,9 +5,6 @@ from tags.models import TaggedItem
 
 def say_hello(request):
     # pylint: disable=no-member 
-    content_type = ContentType.objects.get_for_model(Product)
-    queryset = TaggedItem.objects.select_related('tag').filter(
-        content_type= content_type,
-        object_id = 1 
-    )
+    queryset = TaggedItem.objects.get_tags_for(Product,1)
+
     return render(request, 'hello.html', {'name': 'Wasee', 'result':list(queryset)})
