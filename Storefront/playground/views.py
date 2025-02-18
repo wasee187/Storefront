bb762c0a089cf44773593ba0b3f7cad9ca3ea146
wasee187 <waseesarwar187@gmail.com>
2025-02-18 20:20:11 +0200

    Delete Objects

--- Storefront/playground/views.py
+++ Storefront/playground/views.py
@@ -3,9 +3,9 @@ from store.models import Product, Order, OrderItem, Product, Customer,Collection
 
 def say_hello(request):
     # pylint: disable=no-member    
-    #collection = Collection.objects.get(pk=11)
-    #collection.featured_product = None
-    #collection.save()
+    collection = Collection(pk=11)
+    collection.delete()
 
-    Collection.objects.filter(pk=11).update(featured_product=None)
+    Collection.objects.filter(id__gt = 5).delete()
+
     return render(request, 'hello.html', {'name': 'Wasee'})
