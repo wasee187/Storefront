28b9f8dd70034ee3adc3591387d7b08b80140e28
wasee187 <waseesarwar187@gmail.com>
2025-02-18 20:16:53 +0200

    Updating Objects

--- Storefront/playground/views.py
+++ Storefront/playground/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-
 from store.models import Product, Order, OrderItem, Product, Customer,Collection
 
 def say_hello(request):
     # pylint: disable=no-member    
-    collection = Collection()
-    collection.title = 'Vedio Games'
-    collection.featured_product = Product(pk=1)
-    collection.save()
+    #collection = Collection.objects.get(pk=11)
+    #collection.featured_product = None
+    #collection.save()
 
+    Collection.objects.filter(pk=11).update(featured_product=None)
     return render(request, 'hello.html', {'name': 'Wasee'})
