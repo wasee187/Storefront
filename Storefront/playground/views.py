d3875842187fafca39ae511c2986c96cdb3bd517
wasee187 <waseesarwar187@gmail.com>
2025-02-18 11:20:22 +0200

    Grouping Data

--- Storefront/playground/views.py
+++ Storefront/playground/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
-from django.db.models import Value, F, Func
+from django.db.models import Value, F, Func, Count
 from django.db.models.functions import Concat
 from store.models import Product, Order, Customer
 
 def say_hello(request):
-    # pylint: disable=no-member
-   # queryset = Customer.objects.annotate(
-        #full_name = Func(F('first_name'),Value(' '), F('last_name')), function = 'CONCAT')  
+    # pylint: disable=no-member 
     queryset = Customer.objects.annotate(
-        full_name = Concat('first_name', Value(' '), 'last_name')
+        orders_count = Count('order')
     )
     return render(request, 'hello.html', {'name': 'Wasee', 'result':list(queryset)})
