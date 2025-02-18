70e06c48bce733a784fb9ff69db6ccc85806c5e8
wasee187 <waseesarwar187@gmail.com>
2025-02-18 11:54:56 +0200

    Expression Wrapper

--- Storefront/playground/views.py
+++ Storefront/playground/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from django.db.models import Value, F, Func, Count
+from django.db.models import Value, F, Func, Count, ExpressionWrapper, DecimalField
 from django.db.models.functions import Concat
 from store.models import Product, Order, Customer
 
 def say_hello(request):
     # pylint: disable=no-member 
-    queryset = Customer.objects.annotate(
-        orders_count = Count('order')
+    discounted_price = ExpressionWrapper(F('unit_price') * 0.8 , output_field= DecimalField())
+    queryset = Product.objects.annotate(
+        discounted_price= discounted_price
     )
     return render(request, 'hello.html', {'name': 'Wasee', 'result':list(queryset)})
