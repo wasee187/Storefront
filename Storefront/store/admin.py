77426f303e31ebc9cf57c324916ec3cb36dd503d
wasee187 <waseesarwar187@gmail.com>
2025-02-19 22:55:58 +0200

    providing links to other pages

--- Storefront/store/admin.py
+++ Storefront/store/admin.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.db.models import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 from . import models
 
 
@@ -40,9 +42,15 @@ class CollectionAdmin(admin.ModelAdmin):
     list_per_page = 10
 
     @admin.display(ordering ='products_count')
-    def products_count(self, collection):
-        return collection.products_count
-    def get_queryset(self, request):
+    def products_count(self, collection): #defining products_count method
+        url = (reverse('admin:store_product_changelist')  #url for product page with filtering 
+                + '?'
+                + urlencode({
+                    'collection__id': str(collection.id) #getting collection id in string for filtering
+                }))
+        return format_html('<a href="{}">{}</a>',url, collection.products_count) #html code for the link
+        
+    def get_queryset(self, request): #ovveride get_queryset()
         return super().get_queryset(request).annotate(
             products_count = Count('product')
         )
@@ -54,7 +62,21 @@ class CollectionAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
+    list_display = ['first_name', 'last_name', 'membership', 'orders_count']
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
+
+    @admin.display(ordering='orders_count') #ordering method based on orders_count
+    def orders_count(self, customer): #defining orders_count method
+        url = (reverse("admin:store_order_changelist") #generating url link with reverse method admin:app_model_pagename
+                + '?' 
+                + urlencode({
+                    'customer__id' : str(customer.id) #using urlencode to get customer id for filtering
+                }))
+        return format_html('<a href="{}">{}</a>',url , customer.orders_count) #returning formated url with  customer's order count
+        
+    def get_queryset(self,request): #overriding queryset to get orders_count of each customer
+        return super().get_queryset(request).annotate(
+            orders_count = Count('order')
+        )
