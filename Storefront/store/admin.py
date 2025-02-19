01ef2098afdad59846219a7d2b6296a277b53d84
wasee187 <waseesarwar187@gmail.com>
2025-02-19 23:27:13 +0200

    adding searching option in the listpage

--- Storefront/store/admin.py
+++ Storefront/store/admin.py
@@ -64,8 +64,9 @@ class CollectionAdmin(admin.ModelAdmin):
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership', 'orders_count']
     list_editable = ['membership']
-    ordering = ['first_name', 'last_name']
     list_per_page = 10
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith'] #searching option with case insensitive 
 
     @admin.display(ordering='orders_count') #ordering method based on orders_count
     def orders_count(self, customer): #defining orders_count method
